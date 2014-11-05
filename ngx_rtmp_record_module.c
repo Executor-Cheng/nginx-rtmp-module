13931640fcd9bf4978bada7d4a6de02b6a9fa6df
Thomas Shafer <thomasjshafer@gmail.com>
2014-11-04 16:58:57 -0800

    fat-finger?

--- ngx_rtmp_record_module.c
+++ ngx_rtmp_record_module.c
@@ -1300,7 +1300,7 @@ ngx_rtmp_record_postconfiguration(ngx_conf_t *cf)
     *h = ngx_rtmp_record_avd;
 
     h = ngx_array_push(&cmcf->events[NGX_RTMP_MSG_AMF_META]);
-    *h = ngx_rtmp_record_av;
+    *h = ngx_rtmp_record_avd;
 
     next_publish = ngx_rtmp_publish;
     ngx_rtmp_publish = ngx_rtmp_record_publish;
